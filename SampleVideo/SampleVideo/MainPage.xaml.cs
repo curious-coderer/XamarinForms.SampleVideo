3ac919f64122832324fc9197c14f4f1492a76a02
Daigo <Daigo.Ajamian@gmail.com>
2022-05-05 14:36:08 +0800

    Initial Commit

---
+++ SampleVideo/SampleVideo/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaManager;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace SampleVideo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Handle_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await CrossMediaManager.Current.Play("file:///android_asset/big_buck_bunny.mp4");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private async void Handle_Clicked2(object sender, EventArgs e)
+        {
+            try
+            {
+                var file = await FileSystem.OpenAppPackageFileAsync("big_buck_bunny.mp4");
+                using (var ms = new MemoryStream())
+                {
+                    await file.CopyToAsync(ms);
+                    
+                    
+                    
+                    
+                    
+                    var bytes = ms.ToArray();
+                    using (var ms2 = new MemoryStream(bytes))
+                    {
+                        await CrossMediaManager.Current.Play(ms2, $"{Guid.NewGuid()}.mp4");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
